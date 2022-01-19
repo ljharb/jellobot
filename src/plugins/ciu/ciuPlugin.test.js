eb05d9b6497be4a68d1564aee85e9c6bd9150661
Jordan Harband <ljharb@gmail.com>
2022-01-19 14:04:20 -0800

    [Deps] update `caniuse-db`, `node-fetch`, babel stuff:
     - `@babel/core`, `@babel/generator`, `@babel/parser`, `@babel/plugin-proposal-async-do-expressions`, `@babel/plugin-proposal-async-generator-functions`, `@babel/plugin-proposal-class-properties`, `@babel/plugin-proposal-class-static-block`, `@babel/plugin-proposal-decorators`, `@babel/plugin-proposal-do-expressions`, `@babel/plugin-proposal-dynamic-import`, `@babel/plugin-proposal-export-default-from`, `@babel/plugin-proposal-export-namespace-from`, `@babel/plugin-proposal-function-bind`, `@babel/plugin-proposal-function-sent`, `@babel/plugin-proposal-json-strings`, `@babel/plugin-proposal-logical-assignment-operators`, `@babel/plugin-proposal-nullish-coalescing-operator`, `@babel/plugin-proposal-numeric-separator`, `@babel/plugin-proposal-optional-catch-binding`, `@babel/plugin-proposal-optional-chaining`, `@babel/plugin-proposal-partial-application`, `@babel/plugin-proposal-pipeline-operator`, `@babel/plugin-proposal-private-methods`, `@babel/plugin-proposal-private-property-in-object`, `@babel/plugin-proposal-record-and-tuple`, `@babel/plugin-proposal-throw-expressions`, `@babel/plugin-proposal-unicode-property-regex`, `@babel/plugin-transform-modules-commonjs`, `@babel/plugin-transform-typescript`, `@babel/traverse`

--- src/plugins/ciu/ciuPlugin.test.js
+++ src/plugins/ciu/ciuPlugin.test.js
@@ -14,7 +14,7 @@ it('works with features agents', async () => {
   const output = await testCiu('ciu grid css');
 
   expect(output).toBe(
-    'CSS Grid Layout (level 1) (IE 9~, Edge 16, FF 52, Chrome 57, Opera 44, Safari 10.1, iOS 10.3, Android 95) 96.64% https://caniuse.com/css-grid',
+    'CSS Grid Layout (level 1) (IE 9~, Edge 16, FF 52, Chrome 57, Opera 44, Safari 10.1, iOS 10.3, Android 97) 94.78% https://caniuse.com/css-grid',
   );
 });
 
@@ -22,21 +22,21 @@ it('works with features agents 2', async () => {
   const output = await testCiu('ciu modules');
 
   expect(output).toBe(
-    'JavaScript modules via script tag (Edge 16, FF 60, Chrome 61, Opera 48, Safari 11, iOS 11.0-11.2, Android 95) 94.56% https://caniuse.com/es6-module, see also https://caniuse.com/es6-module-dynamic-import',
+    'JavaScript modules via script tag (Edge 16, FF 60, Chrome 61, Opera 48, Safari 11, iOS 11.0-11.2, Android 97) 92.74% https://caniuse.com/es6-module, see also https://caniuse.com/es6-module-dynamic-import',
   );
 });
 
 it('works with features agents 3', async () => {
   const output = await testCiu('ciu subgrid');
 
-  expect(output).toBe('CSS Subgrid (FF 71) 3.37% https://caniuse.com/css-subgrid');
+  expect(output).toBe('CSS Subgrid (FF 71) 3.58% https://caniuse.com/css-subgrid');
 });
 
 it('works with features agents 4', async () => {
   const output = await testCiu('ciu :has');
 
   expect(output).toBe(
-    ':has() CSS relational pseudo-class  0% https://caniuse.com/css-has',
+    ':has() CSS relational pseudo-class (Safari TP) 0% https://caniuse.com/css-has',
   );
 });
 
@@ -44,7 +44,7 @@ it('works with total usage percent support rounding', async () => {
   const output = await testCiu('ciu keyboardevent-key');
 
   expect(output).toBe(
-    'KeyboardEvent.key (IE 9~, Edge 79, FF 29, Chrome 51, Opera 12.1, Safari 10.1, iOS 10.3, Android 95) 97.98% https://caniuse.com/keyboardevent-key',
+    'KeyboardEvent.key (IE 9~, Edge 79, FF 29, Chrome 51, Opera 12.1, Safari 10.1, iOS 10.3, Android 97) 96.05% https://caniuse.com/keyboardevent-key',
   );
 });
 
@@ -52,6 +52,6 @@ it('works with search', async () => {
   const output = await testCiu('ciu keyboard key');
 
   expect(output).toBe(
-    'KeyboardEvent.key (IE 9~, Edge 79, FF 29, Chrome 51, Opera 12.1, Safari 10.1, iOS 10.3, Android 95) 97.98% https://caniuse.com/keyboardevent-key, see also https://caniuse.com/keyboardevent-which, https://caniuse.com/keyboardevent-location, https://caniuse.com/keyboardevent-code, https://caniuse.com/keyboardevent-charcode',
+    'KeyboardEvent.key (IE 9~, Edge 79, FF 29, Chrome 51, Opera 12.1, Safari 10.1, iOS 10.3, Android 97) 96.05% https://caniuse.com/keyboardevent-key, see also https://caniuse.com/keyboardevent-which, https://caniuse.com/keyboardevent-location, https://caniuse.com/keyboardevent-code, https://caniuse.com/keyboardevent-charcode',
   );
 });
